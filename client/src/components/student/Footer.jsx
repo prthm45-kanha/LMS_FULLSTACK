2ee06381f7e92c3fe3ef17ac304b1fdf39064840
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-07-09 21:06:56 +0530

    donw with dashboard page

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
         </div>
       </div>
     </div>
-    <p className='py-4 text-center text-xs md:text-sm text-white/60'>Copyright 2025 © Bansal Dharmkanta.All Right Reserved.</p>
+    <p className='py-4 text-center text-xs md:text-sm text-white/60'>Copyright 2026 © Bansal Dharmkanta.All Right Reserved.</p>
   </footer>
   )
 }
--- client/src/pages/educator/MyCourses.jsx
+++ client/src/pages/educator/MyCourses.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
 
 const MyCourses = () => {
+  useContext(AppContext)
   return (
     <div>
       <h1>My Courses Page</h1>
